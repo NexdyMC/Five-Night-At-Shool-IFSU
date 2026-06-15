66796ee6e7c42fdad9eeacb52ebffeebf75ec047
NexdyMC <febripratama1786@gmail.com>
2026-06-15 21:28:37 +0700

    add script start screen button to hover

--- script/start_screen.gd
+++ script/start_screen.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var tekstur_horor = $Panel/Night
 @onready var btn_new_game = $Panel/BtnNewGame
 @onready var btn_continue = $Panel/BtnContinue
-
+@onready var btn_exit = $Panel/BtnExit
 func _ready():
 	tekstur_horor.visible = false
 
@@ -14,35 +14,40 @@ func _ready():
 func _process(delta: float) -> void:
 	pass
 
+#region Function Button
 
 func _on_btn_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
-
-
 func _on_btn_continue_pressed() -> void:
-	pass # Replace with function body.
-
-
+	get_tree().change_scene_to_file("res://main.tscn")
 func _on_btn_exit_pressed() -> void:
-	pass # Replace with function body.
-
-
+	get_tree().quit()
 func _on_btn_credit_pressed() -> void:
 	get_tree().change_scene_to_file("res://screen/creadit.tscn")
 
-# Mouse Hover
+# Btn New Game Hover
 func _on_btn_new_game_mouse_entered() -> void:
-	pass
-	
+	btn_new_game.modulate = Color.from_string("cccccccc", Color.WHITE)
 func _on_btn_new_game_mouse_exited() -> void:
-	pass
+	btn_new_game.modulate = Color.from_string("ffffffff", Color.WHITE)
 
-
+# Btn Continue Hover
 func _on_btn_continue_mouse_entered() -> void:
 	tekstur_horor.visible = true
-	#btn_continue.modulate.a = 180
-
-
+	btn_continue.modulate = Color.from_string("cccccccc", Color.WHITE)
 func _on_btn_continue_mouse_exited() -> void:
 	tekstur_horor.visible = false
-	#btn_continue.modulate.a = 255
+	btn_continue.modulate = Color.from_string("ffffffff", Color.WHITE)
+
+# Btn Exit Hover
+func _on_btn_exit_mouse_entered() -> void:
+	btn_exit.modulate = Color.from_string("cccccccc", Color.WHITE)
+func _on_btn_exit_mouse_exited() -> void:
+	btn_exit.modulate = Color.from_string("ffffffff", Color.WHITE)
+
+#Btn Credit Hover
+func _on_btn_credit_mouse_entered() -> void:
+	btn_exit.modulate = Color.from_string("cccccccc", Color.WHITE)
+func _on_btn_credit_mouse_exited() -> void:
+	btn_exit.modulate = Color.from_string("ffffffff", Color.WHITE)
+#endregion
